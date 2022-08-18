5c7bb779e76cfe189ebe5bf1b2c717ecd9295348
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2022-08-18 12:46:40 +0600

    react_practice

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      <Clock locale="bn-BD"/>
+      <Clock/>
     </div>
   );
 }
--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,22 +2,35 @@ import React from "react";
 
 
 class Clock extends React.Component {
-    constructor(props, a){
+
+    state = {date: new Date()};
+
+ /*    Same thing as the "state" but in broad sense
+        constructor(props){
         super(props);
         this.state = {date: new Date()};
     }
-
+ */
     componentDidMount(){
-        setInterval(() => {
-            this.setState({
-                date: new Date(),
-            });
-        }, 1000)
+        this.clockTimer = setInterval(() => this.tick(), 1000)
     }
+
+    componentWillUnmount(){
+        clearInterval(this.clockTimer);
+    }
+
+    tick(){
+        this.setState({
+            date: new Date(),
+        });
+    }
+
+
     render(){
+        const {date} = this.state;
         return(
             <h2 className="heading">
-            <span className="text">{new Date(this.state.date).toLocaleTimeString(this.props.locale)}</span>
+            <span className="text">{date.toLocaleTimeString("bn-BD")}</span>
             </h2>
         )
     };
